f5412d04040ac89b88e11944b9189c8655ddb1a7
koby5i <bobkoby5i@xdeq.com>
2020-11-16 21:49:21 +0100

    add products collection

--- my-fe-app/src/app/home/home.module.ts
+++ my-fe-app/src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './pages/home/home.page';
 import { ProductsPage } from './pages/products/products.page';
+import { ProductsComponent } from './components/products/products.component';
 
 
 @NgModule({
-  declarations: [HomePage, ProductsPage],
+  declarations: [HomePage, ProductsPage, ProductsComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
--- my-fe-app/src/app/home/pages/products/products.page.ts
+++ my-fe-app/src/app/home/pages/products/products.page.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-products',
+  //selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss']
 })
 export class ProductsPage implements OnInit {
 
+  products = [
+    {id:1, sku:'10001001', name: 'Keyboard', price:"100"},
+    {id:2, sku:'10001002', name: 'Mouse', price:"350"},
+    {id:3, sku:'10001003', name: 'Monitor', price:"400"},
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
