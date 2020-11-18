be1ec43180157bbe57cfa8163b137e4351243b03
koby5i <bobkoby5i@xdeq.com>
2020-11-18 02:04:24 +0100

    add service connecto to whapi

--- my-fe-app/src/app/home/pages/products/products.page.ts
+++ my-fe-app/src/app/home/pages/products/products.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/api/api.model';
+import { ProductsService } from '../../services/products.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   //selector: 'app-products',
@@ -8,29 +10,36 @@ import { Product } from 'src/app/api/api.model';
 })
 export class ProductsPage implements OnInit {
 
-  products = [
-    {id:1, sku:'10001001', name: 'Keyboard', price:"100"},
-    {id:2, sku:'10001002', name: 'Mouse', price:"350"},
-    {id:3, sku:'10001003', name: 'Monitor', price:"400"},
-  ];
+  // products = [
+  //   {id:1, sku:'10001001', name: 'Keyboard', price:"100"},
+  //   {id:2, sku:'10001002', name: 'Mouse', price:"350"},
+  //   {id:3, sku:'10001003', name: 'Monitor', price:"400"},
+  // ];
+
+  products: Product[]= [];
+  sub = new Subscription();  
 
   selectedId: number | null = null;
   selectedName: string | null = null;
   selectedPrice: number | null = null;
   
-  constructor() { }
+  constructor(private productsService: ProductsService,) { }
 
   ngOnInit(): void {
     console.log('INIT Contact Page')
+    const s = this.productsService.getProducts().subscribe(products => this.products = products);
+    this.sub.add(s);
   }
+
   ngOnDestroy(){
-    console.log('DESTROY Contact Page')
+    console.log('DESTROY Products Page; going HOME ?')
+    this.sub.unsubscribe()
   }  
+
   handleProductMouseOver(product: Product){
-    this.selectedId = product.id
-    this.selectedName = product.name
+    this.selectedId    = product.id
+    this.selectedName  = product.name
     this.selectedPrice = product.price
-    //this.contacts.push(contact)
   }
 
 }
---
+++ my-fe-app/src/app/home/services/products.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/api/api.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  products: Product[]=[];
+  selectedName: string | null = null;
+  base: string | null = "http://koby5i-spri6ng-boot-intro-wh.herokuapp.com/api";
+
+  constructor(private http: HttpClient,
+    //@Inject(API_BASE_URL) private base:string,
+    ) {
+      console.log('ProductsService Constructor bas='+this.base);
+      //this.base = "http://koby5i-spri6ng-boot-intro-wh.herokuapp.com/api";
+      this.base = "http://localhost:8080/api";
+     }
+
+
+  getProducts(): Observable<Product[]>  {
+    console.log('INIT Contact Page')
+    return this.http.get<Product[]>(this.base+'/items')
+  }  
+
+  getProductById(id:number): Observable<Product> {
+    return this.http.get<Product>(this.base+'/items/'+id)
+    }  
+}
